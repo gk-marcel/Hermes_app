d220410ad46892d0c708a978dbac6d60e6a53718
gk-marcel <gk.marcel@gmail.com>
2017-05-24 16:41:52 +0200

    Repository creation
    1st commit

---
+++ hermes.py
@@ -0,0 +1,45 @@
+#IMPORT FLASK
+from flask import Flask, request, render_template, redirect, url_for
+import hermesJSON
+
+#CREATE HERMES INSTANCE
+hermes = Flask(__name__)
+
+
+#-----PAGE VIEWS-----#
+@hermes.route('/', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        #FORM VALIDATION
+        username = request.form['username']
+        return redirect(url_for('calendar', username=username))
+    else:
+        return render_template('login.html')
+
+@hermes.route('/calendar/<username>')
+def calendar(username):
+    
+    JSONfile = hermesJSON.getJsonExample()
+    
+    #Obtinc una llista de totes les classes i amb les seves caracteristiques
+    llistaClasses = hermesJSON.convertJSON(JSONfile)
+    
+    #files necessaries
+    filaMax = 0
+    for classe in llistaClasses:
+        if classe['startRow'] > filaMax:
+            filaMax = classe['startRow']
+        if classe['endRow'] > filaMax:
+            filaMax = classe['endRow']
+    
+    #Pass to Jinja2
+    return render_template('calendar.html',
+                           username=username,
+                           llistaClasses=llistaClasses,
+                           filaMax=filaMax)
+
+
+
+#------APP INIT-----#
+if __name__=='__main__':
+    hermes.run(debug=True)
---
+++ hermesJSON.py
@@ -0,0 +1,85 @@
+import json
+
+def getJsonExample():
+    with open('JSONexample1.json', 'r') as f:
+        return json.load(f)
+
+def getRows(llistaClasses):
+    llistaHores = []
+    for classe in llistaClasses:
+        llistaHores.append(classe['startHour'][0]*60 + classe['startHour'][1])
+        llistaHores.append(classe['endHour'][0]*60 + classe['endHour'][1])
+    minutMinim = min(llistaHores)
+    for classe in llistaClasses:
+        classe['startRow'] = (classe['startHour'][0]*60 + classe['startHour'][1] - minutMinim)/30
+        classe['endRow'] = (classe['endHour'][0]*60 + classe['endHour'][1] - minutMinim)/30
+    return llistaClasses
+
+def convertJSON(JSONfile):
+    
+    llistaClasses = []
+    classe = {}
+    dies = {u'monday':1, u'tuesday':2, u'wednesday':3, u'thursday': 4, u'friday': 5}
+    
+    llistaAssignatures = JSONfile['subjects']
+    
+    #Creacio dun diccionari pels colors
+    llistaNoms = []
+    for assignatura in llistaAssignatures:
+        llistaNoms.append(assignatura['name'])
+    llistaColors = []
+    for i in range(0,len(llistaNoms)):
+        llistaColors.append('c'+str(i))
+    diccionariColors = dict(zip(llistaNoms, llistaColors))
+    
+    #identificador de una classe concreta
+    identificador = 0 
+    
+    for assignatura in llistaAssignatures:
+        diccionariGrups = assignatura['group']
+        for numeroGrup, classesDelGrup in diccionariGrups.iteritems():
+            for sessio in classesDelGrup:
+                
+                #Nom de la classe
+                classe['name'] = assignatura['name']
+                
+                #Dia de la classe
+                classe['day'] = sessio['day']
+                classe['dayNumber'] = dies[sessio['day']]
+                
+                #Hora de la classe
+                classe['startHour'] = [int(sessio['startHour'].split(':')[0]),int(sessio['startHour'].split(':')[1])]
+                classe['endHour'] = [int(sessio['endHour'].split(':')[0]),int(sessio['endHour'].split(':')[1])]
+                
+                classe['startHourString'] = sessio['startHour'][0:5]
+                classe['endHourString'] = sessio['endHour'][0:5]
+                
+                #COLOR DE LA CLASSE
+                classe['color'] = diccionariColors[classe['name']]
+                
+                #ALTRES
+                classe['classroom'] = sessio['classroom']
+                classe['grup'] = str(numeroGrup)
+                classe['profe'] = 'Marc Garcia'
+                classe['emailProfe'] = 'marc.garcia@gmail.com'
+                classe['codi'] = assignatura['code']
+                classe['id'] = identificador
+                identificador += 1
+                
+                llistaClasses.append(classe.copy())
+    
+    #Calcular ROWS
+    llistaClasses = getRows(llistaClasses)
+    #print llistaClasses
+    return llistaClasses
+
+'''
+JSONfile = getJsonExample()
+    
+#Obtinc una llista de totes les classes i amb les seves caracteristiques
+llistaClasses = convertJSON(JSONfile)
+print llistaClasses
+
+#Calculate necessary timetable rows
+filesNecessaries = getNecessaryRows(llistaClasses)
+'''
